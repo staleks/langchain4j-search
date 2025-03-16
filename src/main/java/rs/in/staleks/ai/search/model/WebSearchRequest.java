c6f5df177eb189f824e521a128b2c5fccd63659e
astoisavljevic <astoisavljevic@jatheon.com>
2025-03-16 23:34:42 +0100

    add jakarta validation

--- src/main/java/rs/in/staleks/ai/search/model/WebSearchRequest.java
+++ src/main/java/rs/in/staleks/ai/search/model/WebSearchRequest.java
@@ -1,10 +1,12 @@
 package rs.in.staleks.ai.search.model;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class WebSearchRequest {
 
+    @NotBlank(message = "Query cannot be blank")
     private String query;
 
 }
