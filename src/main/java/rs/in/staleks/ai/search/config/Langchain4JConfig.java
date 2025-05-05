97d1692935858bbe9ead76f0d31c13518772c680
astoisavljevic <astoisavljevic@jatheon.com>
2025-05-05 19:44:39 +0200

    introduced spring dotEnv and using latest version for langchain4j

--- src/main/java/rs/in/staleks/ai/search/config/Langchain4JConfig.java
+++ src/main/java/rs/in/staleks/ai/search/config/Langchain4JConfig.java
@@ -4,16 +4,22 @@ import dev.langchain4j.rag.content.retriever.ContentRetriever;
 import dev.langchain4j.rag.content.retriever.WebSearchContentRetriever;
 import dev.langchain4j.web.search.WebSearchEngine;
 import dev.langchain4j.web.search.tavily.TavilyWebSearchEngine;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Langchain4JConfig {
+    
+    @Value("${tavily.apiKey}")
+    private String tavilyApiKey;
 
     @Bean
     WebSearchEngine webSearchEngine() {
         return TavilyWebSearchEngine.builder()
-                .apiKey(System.getenv("TAVILY_API_KEY"))
+                .apiKey(tavilyApiKey)
+                .includeRawContent(true)
+                .includeAnswer(true)
                 .build();
     }
 
